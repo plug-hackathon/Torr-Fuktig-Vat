623cea2b20d3dcac4c12b8c07816300d4765fac7
Robin Karlsson <rk222ev@student.lnu.se>
2015-12-13 12:37:36 +0100

    Read data from server

--- client/app/assets/javascripts/pages.js
+++ client/app/assets/javascripts/pages.js
@@ -1,29 +1,36 @@
 window.addEventListener("load", function () {
-  var data = {
-      labels : ["46", "47", "48", "49", "50"],
-          legendTemplate : "<ul class=\"<%=name.toLowerCase()%>-legend\"><% for (var i=0; i<datasets.length; i++){%><li><span style=\"background-color:<%=datasets[i].strokeColor%>\"></span><%if(datasets[i].label){%><%=datasets[i].label%><%}%></li><%}%></ul>"
-          ,
-      datasets : [
-        {
-          fillColor : "rgba(220,220,220,0.5)",
-          strokeColor : "rgba(220,220,220,1)",
-          pointColor : "rgba(220,220,220,1)",
-          pointStrokeColor : "#fff",
-          data : [65,59,90,81,56,55,40]
-        },
-        {
-          fillColor : "rgba(151,187,205,0.5)",
-          strokeColor : "rgba(151,187,205,1)",
-          pointColor : "rgba(151,187,205,1)",
-          pointStrokeColor : "#fff",
-          data : [28,48,40,19,96,27,100]
-        }
-      ]
-    }
+  console.log("load");
 
-    var myNewChart = new Chart($("#canvas").get(0).getContext("2d")).Line(data)
+  var server = "http://188.226.137.177:8000/sensordata"
+  var oReq = new XMLHttpRequest();
+  oReq.addEventListener("load", function (response) {
+    console.log(response);
+    
+    var data = {
+        labels : ["46", "47", "48", "49", "50"],
+        datasets : [
+          {
+            fillColor : "rgba(220,220,220,0.5)",
+            strokeColor : "rgba(220,220,220,1)",
+            pointColor : "rgba(220,220,220,1)",
+            pointStrokeColor : "#fff",
+            data : [65,59,90,81,56,55,40]
+          },
+          {
+            fillColor : "rgba(151,187,205,0.5)",
+            strokeColor : "rgba(151,187,205,1)",
+            pointColor : "rgba(151,187,205,1)",
+            pointStrokeColor : "#fff",
+            data : [28,48,40,19,96,27,100]
+          }
+        ]
+      };
 
+      var myNewChart = new Chart($("#canvas").get(0).getContext("2d")).Line(data)
 
-    console.log(canvas);
+  });
+
+  oReq.open("GET", server);
+  oReq.send();
 
 });
